955bffc6f84d798684ecfeaefc5aba0909ea02d9
nanasuzi <kamizyou3@gmail.com>
2021-09-04 19:27:31 +0900

    tab fisnish

--- js-Tab/tab.js
+++ js-Tab/tab.js
@@ -4,6 +4,8 @@
   const $tab = $doc.getElementById('js-tab');
   const $nav = $tab.querySelectorAll('[data-nav]');
   const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS = 'is-active';
+  const $navLen = $nav.length
   
   //初期化
   const init = () => {
@@ -14,9 +16,31 @@
   //クリックしたら起こるイベント
   const handleClick = (e) => {
     e.preventDefault();
-    console.log('Clicked!');
+
+    //クリックされたnavとそのdataを取得
+    const $this = e.target;
+    const targetVal = $this.dataset.nav;
+    console.log('targetVal', targetVal)
+
+    //対象外のnav,contentすべて一度リセットする
+    let index = 0;
+    while(index < $navLen){
+      $content[index].style.display = 'none'
+      $nav[index].classList.remove(ACTIVE_CLASS);
+      index++;
+    };
+
+    //対象のコンテンツをアクティブ化する
+    $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = "block";
+    $nav[targetVal].classList.add(ACTIVE_CLASS);
+    console.log('$nav[targetVal].classList', $nav[targetVal].classList);
   };
 
-  $nav[0].addEventListener('click', (e) => handleClick(e));
+  //全nav要素に対して関数を適応・発火
+  let index = 0;
+  while(index < $navLen){
+    $nav[index].addEventListener('click', (e) => handleClick(e));
+    index++;
+  }
 
 })();
