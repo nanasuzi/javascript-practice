3e0b7bc3ca65a3a3e1f3e4e7d9b93a1eb2133e16
nanasuzi <kamizyou3@gmail.com>
2021-09-07 16:16:35 +0900

    add function

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -1,7 +1,17 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const ul = document.getElementById("ul");
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   console.log(input.value);
+  add();
 });
+
+function add() {
+  const li = document.createElement("li");
+  li.innerText = input.value;
+  li.classList.add("list-group-item");
+  ul.appendChild(li);
+  input.value = "";
+};
