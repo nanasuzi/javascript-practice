f81f03b08fbea5daecc6583527e48e2a2fa284fd
nanasuzi <kamizyou3@gmail.com>
2021-09-09 16:41:17 +0900

    ToDo add Delete機能

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -26,6 +26,12 @@ function add(todo) {
     const li = document.createElement("li");
     li.innerText = todoText;
     li.classList.add("list-group-item");
+
+    li.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
+      li.remove();
+      saveData();
+    });
     ul.appendChild(li);
     input.value = "";
     saveData();
