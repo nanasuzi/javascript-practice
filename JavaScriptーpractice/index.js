b9f79761ddeceb21daf9667790933a62911a3d02
nanasuzi <kamizyou3@gmail.com>
2021-09-16 19:09:18 +0900

    選択肢・問題文追加

--- JavaScriptーpractice/index.js
+++ JavaScriptーpractice/index.js
@@ -1,5 +1,16 @@
 const question = 'トマトはどれ？';
 const answers = [
-  'トマト', 'ピーマン', 'たまねぎ', 'レタス'
+  'トマト',
+  'ピーマン',
+  'たまねぎ',
+  'レタス'
 ];
 const correct = 'トマト';
+
+
+console.log(document.getElementById('js-question').textContent = question);
+
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
