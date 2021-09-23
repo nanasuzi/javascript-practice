fad98208ceee03d1754e6e45ecac22bfe1dc7889
nanasuzi <kamizyou3@gmail.com>
2021-09-23 23:43:28 +0900

    [完成]

--- JavaScriptーpractice/index.js
+++ JavaScriptーpractice/index.js
@@ -1,21 +1,47 @@
-const question = 'トマトはどれ？';
-const answers = [
-  'トマト',
-  'ピーマン',
-  'たまねぎ',
-  'レタス'
+const quiz = [
+  {
+    question: 'トマトはどれ？',
+    answers: [
+      'トマト',
+      'エリンギ',
+      'トメト',
+      'てっぱん'
+    ],
+    correct: 'トマト'
+  }, {
+    question: 'エリンギはどれ？',
+    answers: [
+      'ポルチーニ',
+      'ペイル',
+      'トマト',
+      'エリンギ'
+    ],
+    correct: 'エリンギ'
+  }, {
+    question: 'pi-manはどれ？',
+    answers: [
+      'pi-man',
+      'ピーマン',
+      'パスタ',
+      'モリオ'
+    ],
+    correct: 'pi-man'
+  }
 ];
-const correct = 'トマト';
 
+const quizLen = quiz.length;
 const $button = document.getElementsByTagName('button')
 let $buttonLen = $button.length
-let buttonIndex = 0;
+let score = 0;
+let quizIndex = 0;
+let handlerIndex = 0;
 
 // クイズの問題文・選択肢定義
 const setUpQuiz = () =>{
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
+  let buttonIndex = 0;
   while(buttonIndex < $buttonLen){
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
@@ -24,14 +50,22 @@ setUpQuiz();
 //正誤判定
 
 const clickHandler = (e) => {
-  if(correct === e.target.textContent){
+  if(quiz[quizIndex].correct === e.target.textContent){
     window.alert('正解')
+    score++;
   } else {
     window.alert('不正解')
   }
+
+  quizIndex++;
+
+  if(quizIndex < quizLen){
+    setUpQuiz();
+  } else {
+    window.alert('終了！あなたの正解数は' + score + '/' + quizLen + 'です')
+  }
 };
 
-let handlerIndex = 0;
 while (handlerIndex < $buttonLen) {
   $button[handlerIndex].addEventListener('click', (e) => {
     clickHandler(e);
